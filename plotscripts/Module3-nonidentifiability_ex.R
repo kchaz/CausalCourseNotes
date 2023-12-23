5fc6f99c79cb208268133af8c4db882a6dac1a8e
KChaz <kec89@cornell.edu>
2023-12-23 01:20:53 -0500

    Scripts for creating plots in notes

---
+++ plotscripts/Module3-nonidentifiability_ex.R
@@ -0,0 +1,35 @@
+library(mnormt)
+
+# covariance matrices
+Sigma1 = diag(2)
+Sigma2 = matrix(c(1,.85,.85,1), nrow = 2)
+
+# generate random draws
+n = 5000
+Y = rnorm(n)
+bvn1 = rmnorm(n, mean = c(0,0), varcov = Sigma1)
+bvn2 = rmnorm(n, mean = c(0,0), varcov = Sigma2)
+bvn3 = cbind(Y,Y)
+sims = list(bvn1,bvn2,bvn3)
+
+# plotting
+par(mfrow = c(1,3))
+par(mar = c(5,5,5,5))
+r = c(0,.85,1)
+for (i in 1:3){
+    mat = sims[[i]]
+    plot(mat[,1],mat[,2], 
+             main = paste("r =", r[i]),
+             cex.main = 3,
+             cex.axis = 2,
+             xlim = c(-5,5),
+             ylim = c(-5,5),
+             cex.lab = 3,
+             xlab = "Y(0)",
+             ylab = "Y(1)",
+             axes = F)
+    Axis(side=1, labels=FALSE)
+    Axis(side=2, labels=FALSE)
+}
+
+
